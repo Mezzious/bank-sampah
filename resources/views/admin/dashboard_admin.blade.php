b9f2586b7a5fbb130030e1d593325ff4f973da82
Mezzious <115684191+Mezzious@users.noreply.github.com>
2024-04-06 12:38:36 +0700

    ubah dashboard

--- resources/views/admin/dashboard_admin.blade.php
+++ resources/views/admin/dashboard_admin.blade.php
@@ -1,4 +1,4 @@
-@extends('layout.app_user')
+@extends('layout.app_admin')
 
 @section('content')
     <!-- Begin Page Content -->
--- resources/views/admin/laporan_jual_admin.blade.php
+++ resources/views/admin/laporan_jual_admin.blade.php
@@ -1,4 +1,4 @@
-@extends('layout.app')
+@extends('layout.app_admin')
 
 @section('content')
     <div class="main">
