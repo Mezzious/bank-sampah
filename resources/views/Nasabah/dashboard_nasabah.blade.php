310dc243bdf756d15f6904735399f13225db2c4c
Mezzious <115684191+Mezzious@users.noreply.github.com>
2024-04-11 13:25:05 +0700

    mengubah route nasabah

--- resources/views/Nasabah/dashboard_nasabah.blade.php
+++ resources/views/Nasabah/dashboard_nasabah.blade.php
@@ -26,7 +26,7 @@
                                     aria-labelledby="topbarUserDropdown" style="border-radius: 10px;">
                                     <li>
                                         <a class="btn btn-block btn-custom mb-2" style="border-radius: 8px;"
-                                            href="{{ route('ganti_password_admin') }}">
+                                            href="{{ route('ganti_password_nasabah') }}">
                                             <i class="bi bi-key-fill"></i> Password
                                         </a>
                                     </li>
