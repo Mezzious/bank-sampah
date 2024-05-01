7505ad944ffe97d6f68d111683ef862756c8cfc5
Mezzious <115684191+Mezzious@users.noreply.github.com>
2024-05-01 09:21:36 +0700

    mencenterkan nota di pengepul dan superadmin

--- resources/views/User/transaksi_jual_user.blade.php
+++ resources/views/User/transaksi_jual_user.blade.php
@@ -116,7 +116,7 @@
                                             <td>10Kg</td>
                                             <td>5000</td>
                                             <td>50000</td>
-                                            <td>
+                                            <td style="text-align: center">
                                                 <a href="#" class="btn btn-primary btn-sm" style="color: white"> <i
                                                         class="bi bi-eye-fill"></i> </a>
                                             </td>
