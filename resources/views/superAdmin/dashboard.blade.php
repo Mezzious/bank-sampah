80277ba907317d317edb7a69f516aa0c3bc31972
hyct18 <calvinprasetyo18@gmail.com>
2024-08-15 23:32:18 +0700

    update responsif

--- resources/views/superAdmin/dashboard.blade.php
+++ resources/views/superAdmin/dashboard.blade.php
@@ -7,35 +7,44 @@
         <link rel="stylesheet" href="./assets/compiled/css/all.view.css">
         <!-- Page Heading -->
         <div class="main">
-            <div class="page-heading">
+            <div class="page-heading mb-4">
                 <div class="row">
-                    <div class="d-flex align-items-center justify-content-between ">
-                        <h2 style="font-size: 30px" class="h2 mb-0 col-4 col-md-2 text-gray-800">Dashboard</h2>
-                        <div class="col-8 col-xl-10 col-lg-9 col-md-8 col-sm-9 d-flex align-items-center justify-content-end">
-                            <div class="dropdown">
-                                <a href="#" id="topbarUserDropdown" class="user-dropdown d-flex align-items-center dropend dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-                                    <div class="text">
-                                        <h6 class="user-dropdown-name">Selamat Datang, {{ $user->name }}</h6>
-                                        <p class="user-dropdown-status text-sm text-muted"></p>
-                                    </div>
-                                </a>
-                                <ul class="dropdown-menu dropdown-menu-center shadow-lg text-center p-3" aria-labelledby="topbarUserDropdown" style="border-radius: 10px;">
-                                    <li>
-                                        <a class="btn btn-block btn-custom mb-2" style="border-radius: 8px;" href="{{ route('ganti_password') }}">
-                                            <i class="bi bi-key-fill"></i> Ganti Password
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <form method="get" action="{{ route('logout') }}">
-                                            <input type="hidden" name="_token" value="Fp6EQq2SXZNoCNVF3DWv21fbnsh5DCjvA7Bgx5UK">
-                                            <span class="text-black d-grid gap-5">
-                                                <button class="btn btn-danger" type="submit" style="border-radius: 8px;">
-                                                    <i class="bi bi-box-arrow-left"></i> Logout
-                                                </button>
-                                            </span>
-                                        </form>
-                                    </li>
-                                </ul>
+                    <div class="col-12 d-flex align-items-center justify-content-between">
+                        <h2 class="h2 mb-0 text-gray-800">Dashboard</h2>
+                        <div class="d-flex align-items-center">
+                            <!-- Menu for larger screens -->
+                            <div class="d-none d-md-flex align-items-center">
+                                <div class="dropdown">
+                                    <a href="#" id="topbarUserDropdown" class="user-dropdown d-flex align-items-center dropend dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                                        <div class="d-flex flex-column ms-2 text-end">
+                                            <h6 class="user-dropdown-name mb-0">Selamat Datang, {{ $user->name }}</h6>
+                                            <p class="user-dropdown-status text-sm text-muted mb-0"></p>
+                                        </div>
+                                    </a>
+                                    <ul class="dropdown-menu dropdown-menu-end shadow-lg p-3" aria-labelledby="topbarUserDropdown" style="border-radius: 10px;">
+                                        <li>
+                                            <a class="btn btn-block btn-custom mb-2" style="border-radius: 8px;" href="{{ route('ganti_password') }}">
+                                                <i class="bi bi-key-fill"></i> Ganti Password
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <form method="get" action="{{ route('logout') }}">
+                                                <input type="hidden" name="_token" value="Fp6EQq2SXZNoCNVF3DWv21fbnsh5DCjvA7Bgx5UK">
+                                                <span class="d-grid gap-2">
+                                                    <button class="btn btn-danger" type="submit" style="border-radius: 8px;">
+                                                        <i class="bi bi-box-arrow-left"></i> Logout
+                                                    </button>
+                                                </span>
+                                            </form>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <!-- Hamburger menu for smaller screens -->
+                            <div class="d-md-none ms-2">
+                                <button class="btn" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasMenu" aria-controls="offcanvasMenu">
+                                    <i class="bi bi-person-circle"></i>
+                                </button>
                             </div>
                         </div>
                     </div>
@@ -43,19 +52,39 @@
             </div>
         </div>
 
-        <div class="row">
+        <!-- Offcanvas Menu for smaller screens -->
+        <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasMenu" aria-labelledby="offcanvasMenuLabel">
+            <div class="offcanvas-header">
+                <h5 class="offcanvas-title" id="offcanvasMenuLabel">Selamat Datang, {{ $user->name }}</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body">
+                <div class="text-center">
+                    <a class="btn btn-block btn-custom mb-2" style="border-radius: 8px;" href="{{ route('ganti_password') }}">
+                        <i class="bi bi-key-fill"></i> Ganti Password
+                    </a>
+                    <form method="get" action="{{ route('logout') }}">
+                        <input type="hidden" name="_token" value="Fp6EQq2SXZNoCNVF3DWv21fbnsh5DCjvA7Bgx5UK">
+                        <button class="btn btn-danger w-100" type="submit" style="border-radius: 8px;">
+                            <i class="bi bi-box-arrow-left"></i> Logout
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+        <div class="row g-3">
             <div class="col-xl-3 col-md-6 mb-4">
                 <div class="card border-left-primary shadow h-100 py-2">
-                    <div class="card-body d-grid" style="width: 100%">
-                        <div class="row d-flex justify-content-between align-items-center">
-                            <div class="col-auto">
-                                <div class="h4 mb-0 font-bold text-gray-800">{{ $totalBerat }} Kg</div>
-                                <div class="text-xs font-bold text-primary text-uppercase mb-1">
-                                    Total Sampah (Kg)</div>
+                    <div class="card-body d-flex justify-content-between align-items-center">
+                        <div>
+                            <div class="h4 mb-0 font-weight-bold text-gray-800">{{ $totalBerat }} Kg</div>
+                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                Total Sampah (Kg)
                             </div>
-                            <div class="col-auto">
-                                <i class="fa-regular fa-trash-can"></i>
-                            </div>
+                        </div>
+                        <div>
+                            <i class="fa-regular fa-trash-can fa-2x text-primary"></i>
                         </div>
                     </div>
                 </div>
@@ -63,33 +92,31 @@
 
             <div class="col-xl-3 col-md-6 mb-4">
                 <div class="card border-left-success shadow h-100 py-2">
-                    <div class="card-body d-flex align-items-center align-center">
-                        <div class="row no-gutters align-items-center">
-                            <div class="col mr-2">
-                                <div class="h4 mb-0 font-bold text-gray-800">Rp {{ $totalPembelian }}</div>
-                                <div class="text-xs font-bold text-success text-uppercase mb-1">
-                                    Total Penjualan Sampah</div>
+                    <div class="card-body d-flex justify-content-between align-items-center">
+                        <div>
+                            <div class="h4 mb-0 font-weight-bold text-gray-800">Rp {{ $totalPembelian }}</div>
+                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                                Total Penjualan Sampah
                             </div>
-                            <div class="col-auto">
-                                <i class="fas fa-dollar-sign fa-2x"></i>
-                            </div>
+                        </div>
+                        <div>
+                            <i class="fas fa-dollar-sign fa-2x text-success"></i>
                         </div>
                     </div>
                 </div>
             </div>
 
             <div class="col-xl-3 col-md-6 mb-4">
-                <div class="card border-left-success shadow h-100 py-2">
-                    <div class="card-body d-flex align-items-center align-center">
-                        <div class="row no-gutters align-items-center">
-                            <div class="col mr-2">
-                                <div class="h4 mb-0 font-bold text-gray-800">Rp {{ $totalPenjualan }}</div>
-                                <div class="text-xs font-bold text-success text-uppercase mb-1">
-                                    Total Pembelian Sampah</div>
+                <div class="card border-left-info shadow h-100 py-2">
+                    <div class="card-body d-flex justify-content-between align-items-center">
+                        <div>
+                            <div class="h4 mb-0 font-weight-bold text-gray-800">Rp {{ $totalPenjualan }}</div>
+                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">
+                                Total Pembelian Sampah
                             </div>
-                            <div class="col-auto">
-                                <i class="fas fa-dollar-sign fa-2x"></i>
-                            </div>
+                        </div>
+                        <div>
+                            <i class="fas fa-dollar-sign fa-2x text-info"></i>
                         </div>
                     </div>
                 </div>
@@ -97,16 +124,15 @@
 
             <div class="col-xl-3 col-md-6 mb-4">
                 <div class="card border-left-warning shadow h-100 py-2">
-                    <div class="card-body d-grid">
-                        <div class="row d-flex justify-content-between align-items-center">
-                            <div class="col mr-2">
-                                <div class="h4 mb-0 font-bold text-gray-800">{{ $totalNasabah }}</div>
-                                <div class="text-xs font-bold text-warning text-uppercase mb-1">
-                                    Total User</div>
+                    <div class="card-body d-flex justify-content-between align-items-center">
+                        <div>
+                            <div class="h4 mb-0 font-weight-bold text-gray-800">{{ $totalNasabah }}</div>
+                            <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
+                                Total User
                             </div>
-                            <div class="col-auto">
-                                <i class="fa-solid fa-users"></i>
-                            </div>
+                        </div>
+                        <div>
+                            <i class="fa-solid fa-users fa-2x text-warning"></i>
                         </div>
                     </div>
                 </div>
@@ -115,7 +141,7 @@
 
         <!-- Content Row -->
         <div class="row">
-            <div class="col-xl-12 col-lg-7">
+            <div class="col-xl-12 col-lg-12">
                 <!-- Line Chart -->
                 <div class="card shadow mb-4">
                     <div class="card-header py-3">
@@ -181,6 +207,8 @@
                     ]
                 },
                 options: {
+                    responsive: true,
+                    maintainAspectRatio: false,
                     scales: {
                         x: { title: { display: true, text: 'Bulan' } },
                         y: { title: { display: true, text: 'Total Berat (Kg)' } }
@@ -212,6 +240,8 @@
                     ]
                 },
                 options: {
+                    responsive: true,
+                    maintainAspectRatio: false,
                     scales: {
                         x: { title: { display: true, text: 'Bulan' } },
                         y: { title: { display: true, text: 'Total (Rp)' } }
